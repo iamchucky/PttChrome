15aa6bf0e26f44077db3736e01ab025653238369
Chuck Yang <yangchuck@gmail.com>
2014-06-14 22:49:48 +0800

    Added test_server for local testing

---
+++ test_server.py
@@ -0,0 +1,10 @@
+from flask import Flask, url_for, render_template
+app = Flask(__name__, static_url_path='')
+
+@app.route("/")
+def root():
+  return render_template('index.html')
+
+if __name__ == "__main__":
+  app.debug = True
+  app.run(port=80)
