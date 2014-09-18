5b78e741d95f85a0b406a1397918dd3b3f62f089
Chuck Yang <yangchuck@gmail.com>
2014-09-18 04:58:38 -0400

    setting the static folder so we no longer need these

--- test_server.py
+++ test_server.py
@@ -1,6 +1,6 @@
 import sys
 from flask import Flask, url_for, render_template
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__, static_url_path='', static_folder='')
 
 useDev = True
 if len(sys.argv) > 1 and sys.argv[1] == 'stable':
@@ -9,12 +9,12 @@ if len(sys.argv) > 1 and sys.argv[1] == 'stable':
 @app.route("/")
 def root():
   if useDev:
-    return render_template('dev.html')
+    return app.send_static_file('dev.html')
   else:
-    return render_template('index.html')
+    return app.send_static_file('index.html')
 
 if __name__ == "__main__":
   if useDev:
-    print 'serving dev'
+    print('serving dev')
   app.debug = True
   app.run(host='0.0.0.0', port=80)
