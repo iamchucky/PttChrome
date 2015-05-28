d3584f282f18d425202b14f030d7615e67066cf1
Chuck Yang <yangchuck@gmail.com>
2015-05-28 22:04:54 +0800

    moved SocketClient to a separate file - moved away from switch case

--- background.js
+++ background.js
@@ -62,108 +62,6 @@ function doPaste() {
 }
 ///
 
-var socketOnRead = {};
-var socketOnReadError = {};
-function SocketClient(spec) {
-  this.host = spec.host;
-  this.port = spec.port;
-
-  // Callback functions.
-  this.callbacks = {
-    connect: spec.onConnect,        // Called when socket is connected.
-    disconnect: spec.onDisconnect,  // Called when socket is disconnected.
-    recv: spec.onReceive,           // Called when client receives data from server.
-    sent: spec.onSent               // Called when client sends data to server.
-  };
-  this.socketId = null;
-}
-
-SocketClient.prototype.connect = function() {
-  var self = this;
-  chrome.sockets.tcp.create({}, function(createInfo) {
-    // onCreate
-
-    self.socketId = createInfo.socketId;
-    if (self.socketId > 0) {
-
-      chrome.sockets.tcp.connect(self.socketId, self.host, self.port, function(resultCode) {
-          if (resultCode < 0) {
-            return console.log('socket connect error');
-          }
-
-          // onConnectComplete
-          // set keepalive with 10 mins delay
-          chrome.sockets.tcp.setKeepAlive(self.socketId, true, 600, function(result) {
-            if (result < 0) {
-              return console.log('socket set keepalive error');
-            }
-
-            socketOnRead[self.socketId] = self._onDataRead.bind(self);
-            socketOnReadError[self.socketId] = self._onDataReadError.bind(self);
-            if (self.callbacks.connect) {
-              //console.log('connect complete');
-              self.callbacks.connect();
-            }
-          });
-
-        });
-    } else {
-      //error('Unable to create socket');
-    }
-  });
-};
-
-SocketClient.prototype.send = function(arrayBuffer) {
-  var self = this;
-  chrome.sockets.tcp.send(this.socketId, arrayBuffer, function(sendInfo) {
-    //this.log("bytesWritten = " + sendInfo.bytesSent);
-    if(sendInfo.resultCode == 0) {
-      // write successfully
-      if (self.callbacks.sent) {
-        self.callbacks.sent();
-      }
-    } else if (sendInfo.resultCode == -15) {
-      // socket is closed
-      console.log('socket '+self.socketId+' is closed');
-      self.disconnect();
-      self.callbacks.disconnect();
-    } else if (sendInfo.resultCode < 0) {
-      // other errors
-      console.log('socket '+self.socketId+' reports resultCode '+sendInfo.resultCode);
-    }
-  });
-};
-
-SocketClient.prototype.disconnect = function() {
-  var self = this;
-  chrome.sockets.tcp.disconnect(this.socketId, function() {
-    chrome.sockets.tcp.close(this.socketId, function() {
-      delete socketOnRead[self.socketId];
-      delete socketOnReadError[self.socketId];
-    });
-  });
-};
-
-SocketClient.prototype._onDataRead = function(data) {
-  if (!this.callbacks.recv) return;
-
-  // Call received callback if there's data in the response.
-  var str = String.fromCharCode.apply(null, new Uint8Array(data));
-  this.callbacks.recv(str);
-};
-
-SocketClient.prototype._onDataReadError = function(resultCode) {
-  if (resultCode == -15) {
-    console.log('socket '+this.socketId+' is closed');
-    this.disconnect();
-    if (this.callbacks.disconnect) {
-      this.callbacks.disconnect();
-    }
-  } else {
-    console.log('socket returned code '+resultCode);
-  }
-};
-
 chrome.sockets.tcp.onReceive.addListener(function(resultInfo) {
   if (socketOnRead[resultInfo.socketId]) {
     socketOnRead[resultInfo.socketId](resultInfo.data);
@@ -179,156 +77,171 @@ chrome.sockets.tcp.onReceiveError.addListener(function(resultInfo) {
 // used for only checking status
 chrome.runtime.onMessageExternal.addListener(function(request, sender, sendResponse) {
   if (request.action && request.action == "status") {
-    sendResponse({"result":true});
-    return;
+    return sendResponse({ result: true });
   }
 });
 
-chrome.runtime.onConnectExternal.addListener(function(port) {
-  port.onMessage.addListener(function(msg) {
-    switch(msg.action) {
-      case 'connect':
-        var so = new SocketClient({
-            host: msg.host,
-            port: msg.port,
+// handler for messages passed between web app and the background
+var messageHandler = {
 
-            onConnect: function() {
-              port.postMessage({ action: "connected" });
-            },
+  connect: function(port, msg) {
+    var so = new SocketClient({
+        host: msg.host,
+        port: msg.port,
 
-            onDisconnect: function() {
-              port.postMessage({action: "disconnected" });
-              port.so = null;
-            },
+        onConnect: function() {
+          port.postMessage({ action: "connected" });
+        },
 
-            onReceive: function(str) {
-              port.postMessage({action: "onReceive", data: str});
-            },
-
-            onSent: null
-          });
-
-        port.so = so;
-        so.connect();
-        break;
-      case 'send':
-        if(port.so && typeof(msg.data) == 'string') {
-          var byteArray = new Uint8Array(msg.data.split('').map(function(x){return x.charCodeAt(0);}));
-          port.so.send(byteArray.buffer);
-        }
-        break;
-      case 'disconnect':
-        if (port.so) {
-          port.so.disconnect();
+        onDisconnect: function() {
+          port.postMessage({ action: "disconnected" });
           port.so = null;
-        }
-        break;
-      case 'copy':
-        if (!msg.data)
-          return;
-        if (!clipHelper) {
-          createClipboardHelper(function() {
-            doCopy(msg.data);
-          });
-        } else {
-          doCopy(msg.data);
-        }
-        break;
-      case 'paste':
-        var result = doPaste();
-        port.postMessage({ action: 'onPasteDone', data: result});
-        break;
-      case 'storage':
-        var stype = msg.type;
-        switch(stype) {
-          case 'set':
-            if (msg.data && msg.data.values)
-              chrome.storage.sync.set(msg.data.values, function() {
-                var lastError = chrome.runtime.lastError;
-                if (lastError) console.log(lastError);
-                if (msg.data && msg.data.logins) {
-                  chrome.storage.local.clear(function() {
-                    var err = chrome.runtime.lastError;
-                    if (err)
-                      console.log(err);
-                    else {
-                      var blacklist = { blacklistedUserIds: msg.data.values.blacklistedUserIds };
-                      if (lastError && lastError.message == 'QUOTA_BYTES_PER_ITEM quota exceeded') {
-                        msg.data.logins['blacklistedUserIds'] = msg.data.values.blacklistedUserIds;
-                        msg.data.values.blacklistedUserIds = '';
-                        chrome.storage.sync.set(msg.data.values);
-                      }
-                      chrome.storage.local.set(msg.data.logins);
-                    }
-                  });
-                }
-              });
-            break;
-          case 'get':
-            chrome.storage.sync.get(null, function(items) {
-              var itemsEmpty = (Object.keys(items).length === 0);
-              if (itemsEmpty) {
-                items = JSON.parse(JSON.stringify(msg.defaults.values));
-                console.log('pref: first time, load default to sync storage');
-                chrome.storage.sync.set(items);
-              }
-              chrome.storage.local.get(null, function(localItems) {
-                var localItemsEmpty = (Object.keys(localItems).length === 0);
-                if (localItemsEmpty) {
-                  localItems = JSON.parse(JSON.stringify(msg.defaults.logins));
-                  chrome.storage.local.set(localItems);
-                }
-                if (localItems.blacklistedUserIds) {
-                  items['blacklistedUserIds'] = localItems.blacklistedUserIds;
-                }
-                var data = {
-                  values: items,
-                  logins: { u: localItems.u, p: localItems.p }
-                };
-                port.postMessage({ action: 'onStorageDone', type: 'get', data: data });
-              });
-            });
-            break;
-          case 'clear':
+        },
+
+        onReceive: function(str) {
+          port.postMessage({ action: "onReceive", data: str });
+        },
+
+        onSent: null
+      });
+
+    port.so = so;
+    so.connect();
+  },
+
+  send: function(port, msg) {
+    if(port.so && typeof(msg.data) == 'string') {
+      var byteArray = new Uint8Array(msg.data.split('').map(function(x) { return x.charCodeAt(0); }));
+      port.so.send(byteArray.buffer);
+    }
+  },
+
+  disconnect: function(port, msg) {
+    if (port.so) {
+      port.so.disconnect();
+      port.so = null;
+    }
+  },
+
+  copy: function(port, msg) {
+    if (!msg.data)
+      return;
+    if (!clipHelper) {
+      createClipboardHelper(function() {
+        doCopy(msg.data);
+      });
+    } else {
+      doCopy(msg.data);
+    }
+  },
+
+  paste: function(port, msg) {
+    var result = doPaste();
+    port.postMessage({ action: 'onPasteDone', data: result});
+  },
+
+  storage: function(port, msg) {
+    var stype = msg.type;
+    
+    if (stype == 'set') {
+
+      if (msg.data && msg.data.values)
+        chrome.storage.sync.set(msg.data.values, function() {
+          var lastError = chrome.runtime.lastError;
+          if (lastError) console.log(lastError);
+          if (msg.data && msg.data.logins) {
             chrome.storage.local.clear(function() {
               var err = chrome.runtime.lastError;
               if (err)
                 console.log(err);
+              else {
+                var blacklist = { blacklistedUserIds: msg.data.values.blacklistedUserIds };
+                if (lastError && lastError.message == 'QUOTA_BYTES_PER_ITEM quota exceeded') {
+                  msg.data.logins['blacklistedUserIds'] = msg.data.values.blacklistedUserIds;
+                  msg.data.values.blacklistedUserIds = '';
+                  chrome.storage.sync.set(msg.data.values);
+                }
+                chrome.storage.local.set(msg.data.logins);
+              }
             });
-            chrome.storage.sync.clear(function() {
-              var err = chrome.runtime.lastError;
-              if (err)
-                console.log(err);
-            });
-            break;
-          default:
-            break;
+          }
+        });
+
+    } else if (stype == 'get') {
+
+      chrome.storage.sync.get(null, function(items) {
+        var itemsEmpty = (Object.keys(items).length === 0);
+        if (itemsEmpty) {
+          items = JSON.parse(JSON.stringify(msg.defaults.values));
+          console.log('pref: first time, load default to sync storage');
+          chrome.storage.sync.set(items);
         }
-        break;
-      case 'newWindow':
-        if (!msg.data)
-          return;
-        
-        if (!clipHelper) {
-          createClipboardHelper(function() {
-            clipHelper.contentWindow.openWindow(msg.data);
-          });
-        } else {
-          clipHelper.contentWindow.openWindow(msg.data);
-        }
-        break;
-      case 'closeAppWindow':
-        if (webview) {
-          webview.closeWindow();
-        }
-        break;
-      case 'getSymFont':
-        port.postMessage({ action: 'onSymFont', data: symFontBase64 });
-        break;
-      default:
-        break;
+        chrome.storage.local.get(null, function(localItems) {
+          var localItemsEmpty = (Object.keys(localItems).length === 0);
+          if (localItemsEmpty) {
+            localItems = JSON.parse(JSON.stringify(msg.defaults.logins));
+            chrome.storage.local.set(localItems);
+          }
+          if (localItems.blacklistedUserIds) {
+            items['blacklistedUserIds'] = localItems.blacklistedUserIds;
+          }
+          var data = {
+            values: items,
+            logins: { u: localItems.u, p: localItems.p }
+          };
+          port.postMessage({ action: 'onStorageDone', type: 'get', data: data });
+        });
+      });
+
+    } else if (stype == 'clear') {
+
+      chrome.storage.local.clear(function() {
+        var err = chrome.runtime.lastError;
+        if (err)
+          console.log(err);
+      });
+      chrome.storage.sync.clear(function() {
+        var err = chrome.runtime.lastError;
+        if (err)
+          console.log(err);
+      });
+    }
+
+  },
+
+  newWindow: function(port, msg) {
+    if (!msg.data)
+      return;
+    
+    if (!clipHelper) {
+      createClipboardHelper(function() {
+        clipHelper.contentWindow.openWindow(msg.data);
+      });
+    } else {
+      clipHelper.contentWindow.openWindow(msg.data);
+    }
+  },
+
+  closeAppWindow: function(port, msg) {
+    if (webview) {
+      webview.closeWindow();
+    }
+  },
+
+  getSymFont: function(port, msg) {
+    port.postMessage({ action: 'onSymFont', data: symFontBase64 });
+  }
+
+};
+
+chrome.runtime.onConnectExternal.addListener(function(port) {
+
+  port.onMessage.addListener(function(msg) {
+    if (messageHandler[msg.action]) {
+      messageHandler[msg.action](port, msg);
     }
   });
+
   port.onDisconnect.addListener(function(msg) {
     if (clipHelper) {
       clipHelper.close();
@@ -339,5 +252,6 @@ chrome.runtime.onConnectExternal.addListener(function(port) {
       port.so = null;
     }
   });
+
 });
 
---
+++ socketClient.js
@@ -0,0 +1,102 @@
+var socketOnRead = {};
+var socketOnReadError = {};
+
+function SocketClient(spec) {
+  this.host = spec.host;
+  this.port = spec.port;
+
+  // Callback functions.
+  this.callbacks = {
+    connect: spec.onConnect,        // Called when socket is connected.
+    disconnect: spec.onDisconnect,  // Called when socket is disconnected.
+    recv: spec.onReceive,           // Called when client receives data from server.
+    sent: spec.onSent               // Called when client sends data to server.
+  };
+  this.socketId = null;
+}
+
+SocketClient.prototype.connect = function() {
+  var self = this;
+  chrome.sockets.tcp.create({}, function(createInfo) {
+    // onCreate
+
+    self.socketId = createInfo.socketId;
+    if (self.socketId > 0) {
+
+      chrome.sockets.tcp.connect(self.socketId, self.host, self.port, function(resultCode) {
+        if (resultCode < 0) {
+          return console.log('socket connect error');
+        }
+
+        // onConnectComplete
+        // set keepalive with 10 mins delay
+        chrome.sockets.tcp.setKeepAlive(self.socketId, true, 600, function(result) {
+          if (result < 0) {
+            return console.log('socket set keepalive error');
+          }
+
+          socketOnRead[self.socketId] = self._onDataRead.bind(self);
+          socketOnReadError[self.socketId] = self._onDataReadError.bind(self);
+          if (self.callbacks.connect) {
+            //console.log('connect complete');
+            self.callbacks.connect();
+          }
+        });
+
+      });
+    } else {
+      //error('Unable to create socket');
+    }
+  });
+};
+
+SocketClient.prototype.send = function(arrayBuffer) {
+  var self = this;
+  chrome.sockets.tcp.send(this.socketId, arrayBuffer, function(sendInfo) {
+    //this.log("bytesWritten = " + sendInfo.bytesSent);
+    if(sendInfo.resultCode == 0) {
+      // write successfully
+      if (self.callbacks.sent) {
+        self.callbacks.sent();
+      }
+    } else if (sendInfo.resultCode == -15) {
+      // socket is closed
+      console.log('socket '+self.socketId+' is closed');
+      self.disconnect();
+      self.callbacks.disconnect();
+    } else if (sendInfo.resultCode < 0) {
+      // other errors
+      console.log('socket '+self.socketId+' reports resultCode '+sendInfo.resultCode);
+    }
+  });
+};
+
+SocketClient.prototype.disconnect = function() {
+  var self = this;
+  chrome.sockets.tcp.disconnect(this.socketId, function() {
+    chrome.sockets.tcp.close(this.socketId, function() {
+      delete socketOnRead[self.socketId];
+      delete socketOnReadError[self.socketId];
+    });
+  });
+};
+
+SocketClient.prototype._onDataRead = function(data) {
+  if (!this.callbacks.recv) return;
+
+  // Call received callback if there's data in the response.
+  var str = String.fromCharCode.apply(null, new Uint8Array(data));
+  this.callbacks.recv(str);
+};
+
+SocketClient.prototype._onDataReadError = function(resultCode) {
+  if (resultCode == -15) {
+    console.log('socket '+this.socketId+' is closed');
+    this.disconnect();
+    if (this.callbacks.disconnect) {
+      this.callbacks.disconnect();
+    }
+  } else {
+    console.log('socket returned code '+resultCode);
+  }
+};
