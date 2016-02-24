50e50d705eff9f78756aae26f756405766650a20
I-Ta Tsai@MBPR <ittsai@gmail.com>
2016-02-24 17:30:13 +0800

    can embed flic.kr form of pictures.

--- js/term_view.js
+++ js/term_view.js
@@ -1147,27 +1147,13 @@ TermView.prototype = {
           self.picPreview.style.display = 'block';
         }
         self.picPreviewShouldShown = true;
-      }
+      };
       var found_flickr = elem.getAttribute('href').match('flic\.kr\/p\/\(\\w\+\)');
       if (found_flickr) {
-        function base58_decode( snipcode )
-        {
-            var alphabet = '123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ' ;
-            var num = snipcode.length ;
-            var decoded = 0 ;
-            var multi = 1 ;
-            for ( var i = (num-1) ; i >= 0 ; i-- )
-            {
-                decoded = decoded + multi * alphabet.indexOf( snipcode[i] ) ;
-                multi = multi * alphabet.length ;
-            }
-            return decoded;
-        }
-
         var flickrBase58Id = found_flickr[1];
         var flickrPhotoId = base58_decode(flickrBase58Id);
         elem.setAttribute('data-base58-id', flickrBase58Id);
-        //console.log("flickr shorten url. base58 = " + flickrBase58Id + '   id = ' + flickrPhotoId);
+        elem.setAttribute('data-photo-id', flickrPhotoId);
 
         return function(e) {
           var currBase58 = self.picPreview.getAttribute('data-base58-id');
@@ -1179,11 +1165,9 @@ TermView.prototype = {
             self.picLoading.style.display = 'block';
             var flickrApi = "https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=c8c95356e465b8d7398ff2847152740e&photo_id="+flickrPhotoId+"&format=json&jsoncallback=?";
             $.getJSON(flickrApi, function(data){
-              //console.log(data);
               if (data.photo) {
                 var p = data.photo;
                 var src = "https://farm"+p.farm+".staticflickr.com/"+p.server+"/"+p.id+"_"+p.secret+".jpg";
-                //console.log("src = " + src);
                 if (self.picPreviewAjaxLoading) {
                   setPicPreviewSrc(src);
                 }
@@ -1191,22 +1175,12 @@ TermView.prototype = {
               }
             });
           }
-        }
+        };
       } else {
         return function(e) {
           var href = elem.getAttribute('href');
           var type = elem.getAttribute('type');
           var src = (type == 'p') ? href : (href.indexOf('imgur.com') > 0) ? href.replace('http://imgur.com', 'http://i.imgur.com') + '.jpg' : href + '@.jpg';
-          /*
-          var currSrc = self.picPreview.getAttribute('src');
-          if (src !== currSrc) {
-            self.picLoading.style.display = 'block';
-            self.picPreview.setAttribute('src', src);
-          } else {
-            self.picPreview.style.display = 'block';
-          }
-          self.picPreviewShouldShown = true;
-          */
           setPicPreviewSrc(src);
         };
       }
@@ -1376,24 +1350,46 @@ TermView.prototype = {
   },
 
   embedPicAndVideo: function() {
-    var aNodes = $(".main a[type='p'], .main a[href^='http://imgur.com/']").not("a[href^='http://imgur.com/a/']");
+    var aNodes = $(".main a[type='p'], .main a[href^='http://imgur.com/'], .main a[href^='https://flic.kr/']").not("a[href^='http://imgur.com/a/']");
+    var getPhotoInfoCallback = function(data){
+      if (data.photo) {
+        var p = data.photo;
+        var src = "https://farm"+p.farm+".staticflickr.com/"+p.server+"/"+p.id+"_"+p.secret+".jpg";
+        var theANodes = $('a[data-photo-id="'+p.id+'"]');
+        var imgNode = document.createElement('img');
+        imgNode.setAttribute('class', 'easyReadingImg');
+        imgNode.setAttribute('src', src);
+        imgNode.style.webkitTransform = 'scale('+Math.floor(1/this.scaleX*100)/100+','+Math.floor(1/this.scaleY*100)/100+')';
+        theANodes.parent().append(imgNode);
+        theANodes.attr('view_shown', 'true');
+      }
+    };
+
     for (var i = 0; i < aNodes.length; ++i) {
       var aNode = aNodes[i];
       if (aNode.getAttribute('view_shown')) {
         continue;
       }
       var href = aNode.getAttribute('href');
-      var type = aNode.getAttribute('type');
-      var src = (type == 'p') ? href : (href.indexOf('imgur.com') > 0) ? href.replace('http://imgur.com', 'http://i.imgur.com') + '.jpg' : '';
-      if (src) {
-        var imgNode = document.createElement('img');
-        imgNode.setAttribute('class', 'easyReadingImg');
-        imgNode.setAttribute('src', src);
-        imgNode.style.webkitTransform = 'scale('+Math.floor(1/this.scaleX*100)/100+','+Math.floor(1/this.scaleY*100)/100+')';
-        aNode.parentNode.appendChild(imgNode);
-      }
+      var found_flickr = href.match('flic\.kr\/p\/\(\\w\+\)');
+      if (found_flickr) {
+        var flickrBase58Id = found_flickr[1];
+        var flickrPhotoId = base58_decode(flickrBase58Id);
+        var flickrApi = "https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=c8c95356e465b8d7398ff2847152740e&photo_id="+flickrPhotoId+"&format=json&jsoncallback=?";
+        $.getJSON(flickrApi, getPhotoInfoCallback);
+      } else {
+        var type = aNode.getAttribute('type');
+        var src = (type == 'p') ? href : (href.indexOf('imgur.com') > 0) ? href.replace('http://imgur.com', 'http://i.imgur.com') + '.jpg' : '';
+        if (src) {
+          var imgNode = document.createElement('img');
+          imgNode.setAttribute('class', 'easyReadingImg');
+          imgNode.setAttribute('src', src);
+          imgNode.style.webkitTransform = 'scale('+Math.floor(1/this.scaleX*100)/100+','+Math.floor(1/this.scaleY*100)/100+')';
+          aNode.parentNode.appendChild(imgNode);
+        }
 
-      aNode.setAttribute('view_shown', 'true');
+        aNode.setAttribute('view_shown', 'true');
+      }
     }
 
     var vNodes = document.querySelectorAll(".main a");
@@ -1728,3 +1724,18 @@ TermView.prototype = {
   }
 
 };
+
+// To decode base58 of flickr photo id
+// ref: https://www.flickr.com/groups/51035612836@N01/discuss/72157616713786392/72157620931323757
+function base58_decode(snipcode)
+{
+    var alphabet = '123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ';
+    var num = snipcode.length;
+    var decoded = 0;
+    var multi = 1;
+    for (var i = (num-1); i >= 0; i--) {
+        decoded = decoded + multi * alphabet.indexOf(snipcode[i]);
+        multi = multi * alphabet.length;
+    }
+    return decoded;
+}
